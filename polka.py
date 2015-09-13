4ff21c3be2d668678cfd245ff359f9be3360517d
Stephen Zhu <shzhu@umich.edu>
2015-09-13 02:59:50 -0400

    matches up to 4000

--- polka.py
+++ polka.py
@@ -1,6 +1,6 @@
 import requests
 import csv
-
+import time
 
 apikeys = ['9543874b-46f8-4978-b005-3f18511fed2b',
 		   'c974f3f8-e0d3-4ab8-8a16-a374ef687078',
@@ -17,17 +17,6 @@ apikeys = ['9543874b-46f8-4978-b005-3f18511fed2b',
 		   '19f1278e-4d80-4096-9090-7c8896512432',
 		   '7c9cb67e-ee07-4b8f-96cf-8b16d93f708e'
 		  ]
-
-#{
-#matchId
-#matchDuration
-#matchVersion
-#blueChampions = []
-#redChampions = []
-#blueBans = []
-#purpleBans = []
-#winner = 
-#}
 
 keyCounter = 0
 
@@ -49,6 +38,12 @@ def getMatchInfo(matchId, outcsv, outfile):
 	req = 'https://na.api.pvp.net/api/lol/na/v2.2/match/'+str(matchId) + '?api_key='+ key
 
 	result = requests.get(req)
+	while result.status_code == 429:
+		print result.headers
+		print result
+		time.sleep(2)
+		result = requests.get(req)
+		
 	if result.status_code != 200:
 		print result.status_code
 		print 'mid:', matchId
@@ -78,15 +73,17 @@ def getMatchInfo(matchId, outcsv, outfile):
 
 	for team in res['teams']:
 		if team['teamId'] == 100:
-			for banChamp in team['bans']:
-				blueBans.append(banChamp['championId'])
+			if 'bans' in team:
+				for banChamp in team['bans']:
+					blueBans.append(banChamp['championId'])
 			while len(blueBans) < 3:
 				blueBans.append('')
 		else:
 			if team['winner'] == True:
 				redWinner = 1
-			for banChamp in team['bans']:
-				redBans.append(banChamp['championId'])
+			if 'bans' in team:
+				for banChamp in team['bans']:
+					redBans.append(banChamp['championId'])
 			while len(redBans) < 3:
 				redBans.append('')
 
@@ -103,22 +100,29 @@ def getMatchInfo(matchId, outcsv, outfile):
 
 
 
-def runFromCsv(csvfilename, outcsv, outfile):
+def runFromCsv(csvfilename, outcsv, outfile, begRow, endRow):
 	with open(csvfilename, 'rb') as csvfile:
 		idReader = csv.reader(csvfile)
 		#remove header
 		next(idReader)
+		for i in range(1, begRow):
+			next(idReader)
+
+		it = 0
 		for row in idReader:
+			if begRow + it > endRow:
+				break
 			matchId = row[0]
 			getMatchInfo(matchId, outcsv, outfile)
+			it += 1
 
-matchesOutFile = open('matches.csv', 'wb')
+matchesOutFile = open('matches3000-3999.csv', 'wb')
 matchCsvWriter = csv.writer(matchesOutFile)
 matchCsvWriter.writerow(['MatchId', 'MatchDuration', 'MatchVersion', 'BluePick1', 
 	'BluePick2','BluePick3','BluePick4','BluePick5', 'RedPick1', 'RedPick2', 'RedPick3',
 	'RedPick4', 'RedPick5', 'BlueBan1', 'BlueBan2', 'BlueBan3', 'RedBan1', 'RedBan2', 'RedBan3',
 	'RedWinner'])
-runFromCsv('matchIdsTop2000.csv', matchCsvWriter, matchesOutFile)
+runFromCsv('matchIds.csv', matchCsvWriter, matchesOutFile, 3000,3999)
 
 #getMatchInfo(1948599197, matchCsvWriter, matchesOutFile)
 
